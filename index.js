abeb4c7536dc388c2f4f28351e36cdaec94c7adf
Chiijlaw <chiijlaw@berkeley.edu>
2018-09-21 20:55:48 -0700

    Imported url

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = 3000;
+const url = require("url");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/:id", express.static(path.join(__dirname, "public")));
